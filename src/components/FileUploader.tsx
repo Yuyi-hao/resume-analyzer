b13d26a7737a66a9b47260a26db9f5c110f46b59
Yuyi-hao <roshanisingh2354@gmail.com>
2025-07-20 16:32:13 +0530

    [CHG] Import change

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
-import { formateFileSize } from "../utils/utils";
+import { formateFileSize } from "../lib/utils";
 
 interface FileUploaderProps{
     onFileSelect?: (file: File|null) => void;
